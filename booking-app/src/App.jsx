dd1db08702197186505b6743c3afda0f20014713
peesrikaew <6511130046@mut.ac.th>
2024-12-05 22:42:34 +0700

    test20 edit min.js

--- booking-app/src/App.jsx
+++ booking-app/src/App.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-// import axios from 'axios';
+import axios from 'axios';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 // import Header from '../../go/headbar';
 import Sidebar from './assets/component/sidebar';
