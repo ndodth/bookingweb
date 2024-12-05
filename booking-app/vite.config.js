caba768b9b53af3f3af76e7fcc8cddfe585e2bd2
peesrikaew <6511130046@mut.ac.th>
2024-12-05 17:33:45 +0700

    test9

--- booking-app/vite.config.js
+++ booking-app/vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
