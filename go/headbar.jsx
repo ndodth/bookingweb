a3e76eb33e053e9a336675f034e49a04eed61506
peesrikaew <6511130046@mut.ac.th>
2024-12-05 17:30:44 +0700

    test7

--- go/headbar.jsx
+++ go/headbar.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 import '../css/bootstrap.min.css';
-import '../js/bootstrap.js';
+import '../booking-app/src/assets/js/bootstrap.js';
 import mut from '../pic/mut.png';
 
 function Header() {
