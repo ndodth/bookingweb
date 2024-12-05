e64a8d15598a3aa0d50b1cb2951ce6e7b9325dc4
peesrikaew <6511130046@mut.ac.th>
2024-12-05 16:37:38 +0700

    Please

--- booking-app/vite.config.js
+++ booking-app/vite.config.js
@@ -1,9 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import commonjs from '@rollup/plugin-commonjs';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(),  commonjs(), // เพิ่ม CommonJS Plugin
+  ],
+  optimizeDeps: {
+    include: ['@popperjs/core'], // บังคับแปลง @popperjs/core
+  },
   server: {
     port: 4173,  // หรือพอร์ตที่คุณเลือก
     host: true,   // เปิดให้สามารถเข้าถึงจากภายนอก
